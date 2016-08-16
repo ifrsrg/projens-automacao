1f83f73938121cdf9d4715b415b39ea222e0be32
marciojrtorres <marciojrtorres@gmail.com>
2016-08-16 13:32:47 -0300

    agora foi o dataCatcher

--- beta-0.0.9/work-it/static/dataCatcher.js
+++ beta-0.0.9/work-it/static/dataCatcher.js
@@ -6,43 +6,44 @@
 */
 
 //metadata = []
+(function() {
+    var tbody = document.querySelector('tbody');
+    console.log(tbody);
 
-var tbody = document.querySelector('tbody');
-console.log(tbody);
-
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
+    function readTextFile(file, callback) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.overrideMimeType("application/json");
+        rawFile.open("GET", file, true);
+        rawFile.onreadystatechange = function() {
+            if (rawFile.readyState === 4 && rawFile.status == "200") {
+                callback(rawFile.responseText);
+            }
         }
-    }
-    rawFile.send(null);
-}
-
-readTextFile("/static/dumbster.json", function(text){
-    var data = JSON.parse(text);
-    // metadata = data;
-    // console.log(data);
-    
-    for (var i = 0; i < data.length; i++) {
-        var tr = document.createElement('tr');
-
-        var td = document.createElement('td');
-        tr.appendChild(td);
-        td.innerText = data[i].date;
-
-        var td = document.createElement('td');
-        tr.appendChild(td);
-        td.innerText = data[i].umid;
-
-        var td = document.createElement('td');
-        tr.appendChild(td);
-        td.innerText = data[i].temp;
-
-        tbody.appendChild(tr);
+        rawFile.send(null);
     }
 
-});
+    readTextFile("/static/dumbster.json", function(text){
+        var data = JSON.parse(text);
+        // metadata = data;
+        // console.log(data);
+        
+        for (var i = 0; i < data.length; i++) {
+            var tr = document.createElement('tr');
+
+            var td = document.createElement('td');
+            tr.appendChild(td);
+            td.innerText = data[i].date;
+
+            var td = document.createElement('td');
+            tr.appendChild(td);
+            td.innerText = data[i].umid;
+
+            var td = document.createElement('td');
+            tr.appendChild(td);
+            td.innerText = data[i].temp;
+
+            tbody.appendChild(tr);
+        }
+
+    });
+}());
